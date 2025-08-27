89a312b54fa9d156bf1ec2aa2787dfe15b5dc482
Aliyaan <smdaliyaan@gmail.com>
2025-08-27 23:22:17 +0530

    Add server setup and environment configuration

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
+const server = express();
+let PORT = 3000;
+
+mongoose.connect(process.env.DB_LOCATION,{
+    autoIndex: true
+})
+
+server.post
+
+server.listen(PORT,() => {
+    console.log('listening on port ->' + PORT);
+})
